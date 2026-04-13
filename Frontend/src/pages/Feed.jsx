365899cf8975005b1b2fd5101ee9fb4f0a588342
Nitin <nitinsharmaop828@gmail.com>
2026-04-13 14:29:00 +0530

    colpleted frontend

--- Frontend/src/pages/Feed.jsx
+++ Frontend/src/pages/Feed.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Feed = () => {
+ 
+    const[posts,setposts] =useState([]);
+
   return (
-    <div>
-      
-    </div>
+   <section className='feed-section'>
+    {
+        posts.length > 0 ? (
+            posts.map((posts) => (
+                 <div key={posts._id} className='post-card'>
+                    <img src={posts.img} alt={posts.caption} />
+                    <p>{posts.caption}</p>
+                 </div>
+            ))
+        ) : (
+            <h1>No Posts Available</h1>
+        )
+        }
+ 
+   </section>
   )
 }
 
